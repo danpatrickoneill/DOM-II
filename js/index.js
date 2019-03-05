0820b36339827931d0b040458366232fa05d0fdc
danpatrickoneill <dan47573@gmail.com>
2019-03-05 15:01:53 -0500

    Initial commit - added event listener to all images

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+const images = document.querySelectorAll('img');
+console.log(images)
+
+images.forEach(img => img.addEventListener('mouseover', function(event) {
+  event.target.style.opacity = .5;
+}))
+
+images.forEach(img => img.addEventListener('mouseleave', function(event) {
+  event.target.style.opacity = 1;
+}))
